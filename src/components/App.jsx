42f382b72122a346f8e99a122d57747c3de74a7c
Pavel_soundNotLight <105560877+PavTrap@users.noreply.github.com>
2023-05-12 14:22:02 +0300

    localStarage-componentDidMount/componentDidUpdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -75,11 +75,24 @@ export class App extends Component {
   };
 
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts)
 
-
+    if(parsedContacts){
+      this.setState({contacts: parsedContacts})
+    }
+  };
+  componentDidUpdate(prevProps, prevState){
+    
+    if(this.state.contacts !== prevState.contacts){
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
 
 
   render() {
+    // console.log('App render')
     const { filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
   
