bb6b95ef1af56522bfda629a22aa9d1baf981588
Pavel_soundNotLight <105560877+PavTrap@users.noreply.github.com>
2023-05-12 14:33:11 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -74,6 +74,7 @@ export class App extends Component {
     );
   };
 
+  
 
   componentDidMount() {
     const contacts = localStorage.getItem('contacts');
@@ -84,7 +85,7 @@ export class App extends Component {
     }
   };
   componentDidUpdate(prevProps, prevState){
-    
+
     if(this.state.contacts !== prevState.contacts){
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
     }
